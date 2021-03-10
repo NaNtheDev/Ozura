a0f9ea54de1d9f076d69e03aee9e124ad54a4b7c
667 <52248252+NaNtheDev@users.noreply.github.com>
2021-03-10 20:28:52 +0100

    Update main.js

--- Ozura_Manager/main.js
+++ Ozura_Manager/main.js
@@ -1,3 +1,11 @@
+/* Script by: NaN 
+*  Discord: NaN#7070
+*  Github: https://github.com/NaNtheDev
+*  Repos: https://github.com/NaNtheDev/Ozura
+*  Apache-License 2.0
+*  Reuse permited, but need this following tag
+*/
+
 const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
